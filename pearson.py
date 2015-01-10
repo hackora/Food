6a5e2542d68529830e1d5baf692c4a51019b8a4d
apas <apostolos.me@gmail.com>
2015-01-10 11:49:19 +0100

    working pearson w 500 recipes

--- pearson.py
+++ pearson.py
@@ -3,9 +3,9 @@
 import requests, json
 
 url = "http://api.pearson.com:80/kitchen-manager/v1/recipes?"
-payload = {'ingredients-any': 'spinach'}
-res = requests.get(url, params=payload)
-# res.json()
+payload = {'ingredients-any': 'spinach', 'limit': 3000}
+payload2 = {'limit': 3000}
+res = requests.get(url, params=payload2)
 
 with open('data.json', 'w') as outfile:
   json.dump(res.json(), outfile)
