2e6d1e403b88afa0e565f2c73c50ca3e5c718dfa
apas <apostolos.me@gmail.com>
2015-01-10 20:45:06 +0100

    app fix?

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def application(environ, start_response):
 
    return [response_body]
 
-httpd = make_server('localhost', 8051, application)
+httpd = make_server('localhost', int(os.environ.get('$PORT', 5000)), application)
 # Now it is serve_forever() in instead of handle_request().
 # In Windows you can kill it in the Task Manager (python.exe).
 # In Linux a Ctrl-C will do it.
