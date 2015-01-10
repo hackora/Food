0b982139861e816d3b48cf92c0ba8b8c6cc8164f
apas <apostolos.me@gmail.com>
2015-01-10 12:12:33 +0100

    redux

--- pearson.py
+++ pearson.py
@@ -3,9 +3,8 @@
 import requests, json
 
 url = "http://api.pearson.com:80/kitchen-manager/v1/recipes?"
-payload = {'ingredients-any': 'spinach', 'limit': 3000}
-payload2 = {'limit': 3000}
-res = requests.get(url, params=payload2)
+payload = {'limit': 500}
+res = requests.get(url, params=payload)
 
 with open('data.json', 'w') as outfile:
   json.dump(res.json(), outfile)
